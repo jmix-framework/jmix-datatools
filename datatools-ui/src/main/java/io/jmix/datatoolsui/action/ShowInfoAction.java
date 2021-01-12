8c6c5fdd93ffe6e0e58dbda8781afb2e4a9c1654
Gleb Gorelov <gorelov@haulmont.com>
2021-01-12 14:18:58 +0400

    Implement System Information action Haulmont/jmix-ui_64

---
+++ datatools-ui/src/main/java/io/jmix/datatoolsui/action/ShowInfoAction.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2019 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jmix.datatoolsui.action;
+
+import io.jmix.core.Messages;
+import io.jmix.datatoolsui.screen.systeminfo.SystemInfoWindow;
+import io.jmix.ui.Screens;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.action.ActionType;
+import io.jmix.ui.action.list.SecuredListAction;
+import io.jmix.ui.component.Component;
+import io.jmix.ui.component.ComponentsHelper;
+import io.jmix.ui.meta.StudioAction;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@StudioAction(category = "List Actions",
+        description = "Displays a dialog window with detailed information about the selected entity")
+@ActionType(ShowInfoAction.ID)
+public class ShowInfoAction extends SecuredListAction implements Action.ExecutableAction {
+
+    public static final String ID = "showSystemInfo";
+
+    public ShowInfoAction() {
+        this(ID);
+    }
+
+    public ShowInfoAction(String id) {
+        super(id);
+    }
+
+    @Autowired
+    public void setMessages(Messages messages) {
+        setCaption(messages.getMessage("table.showInfoAction"));
+    }
+
+    @Override
+    public void actionPerform(Component component) {
+        // if standard behaviour
+        if (!hasSubscriptions(ActionPerformedEvent.class)) {
+            execute();
+        } else {
+            super.actionPerform(component);
+        }
+    }
+
+    @Override
+    public void execute() {
+        if (target == null) {
+            throw new IllegalStateException("ShowInfoAction target is not set");
+        }
+
+        Object selectedItem = target.getSingleSelected();
+        if (selectedItem != null) {
+            showInfo(selectedItem, target);
+        }
+    }
+
+    public void showInfo(Object entity, Component.BelongToFrame component) {
+        Screens screens = ComponentsHelper.getScreenContext(component)
+                .getScreens();
+
+        SystemInfoWindow screen = screens.create(SystemInfoWindow.class);
+        screen.setEntity(entity);
+
+        screen.show();
+    }
+}
